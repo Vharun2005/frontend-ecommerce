8fac38c7f412db8ad8ec4eb7423b69cccf57db2f
Vharun2005 <vharunsivaraj06@gmail.com>
2024-12-27 16:06:28 +0530

    Update AppBar.js

--- src/AppBar.js
+++ src/AppBar.js
@@ -5,7 +5,6 @@ import Navbar from 'react-bootstrap/Navbar';
 import { FaShoppingCart } from "react-icons/fa";
 import { FaOpencart } from "react-icons/fa"
 import { Link } from 'react-router-dom';
-import Search from './Search';
 
 
 const AppBar = () => {
@@ -13,14 +12,16 @@ const AppBar = () => {
 
   <Navbar expand="lg" className="bg-body-secondary" id='nav'>
     <Container>
-       <h2 className='fs-1 logo-font log fw-bold'><span><FaOpencart className='text-primary'/> FuryCart</span></h2>
+       <h2 className='fs-1  log fw-bold'><span><FaOpencart className='text-primary '/>FuryCart</span></h2>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav" className='justify-content-end' >
         <Nav className='text-center'>
           
           <Link to="/" className='fs-5 link me-4 ms-2 fw-medium  hover-effect mt-2'>Home</Link>
           <Link className='link' to='/cart'><p  className='fs-5 me-3 ms-2 mt-2  fw-medium  hover-effect' md={1}><FaShoppingCart/>Cart</p></Link>
-
+          <div className='text-center'>
+          <Link to={'/login'} className='link'><button className='login-btn fw-medium'>LogIn</button></Link>
+          </div>
         </Nav>
       </Navbar.Collapse>
     </Container>
